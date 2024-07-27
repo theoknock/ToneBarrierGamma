43c772f50fbb7698fff4cf94bb2a59a56dcbbe9e
James Alan Bush <jamesalanbush1972@gmail.com>
2024-07-27 09:05:13 -0400

    Initial Commit

---
+++ ToneBarrierGamma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToneBarrierGamma
+//
+//  Created by Xcode Developer on 7/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToneBarrierGamma/ToneBarrierGammaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneBarrierGammaApp.swift
+//  ToneBarrierGamma
+//
+//  Created by Xcode Developer on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ToneBarrierGammaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
